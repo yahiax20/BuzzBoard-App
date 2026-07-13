036609d6c640e0d56404838a808fb8ec4deb1b5a
yahiaxx20 <yahia.hamdy6@gmail.com>
2026-07-13 19:00:10 +0200

    edit config.js file

--- frontend/public/config.docker.js
+++ frontend/public/config.docker.js
@@ -1,3 +1,3 @@
 // For docker-compose: browser calls backend on host ports
-window.REACTIONS_SVC_URL = 'http://localhost:8081';
-window.MOOD_SVC_URL = 'http://localhost:8082';
+window.REACTIONS_SVC_URL = 'http://107.22.166.235:8081';
+window.MOOD_SVC_URL = 'http://107.22.166.235:8082';
